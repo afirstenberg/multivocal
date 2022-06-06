9d42ff70bb15849cb2b81cec46dac15d689d501b
afirstenberg <github@addventure.com>
2022-06-06 07:35:38 -0400

    Use function name as name by default. Better handle if undefined or non-object are returned.

--- test/testNamedSequence.js
+++ test/testNamedSequence.js
@@ -11,9 +11,13 @@ seq.forEach( item => {
 
 console.log(seq.names());
 
+function blah(env){env.blah='blah'; return env;}
+
 const f = new NamedFunctionSequence();
-const env = {};
-f.add( env => console.log('a') );
+const env = {x:'x'};
+f.add( env => env.a = 'a' );
+f.add( blah );
 f.add( {After:'First', Name:'Bravo', fn:env => console.log('b')} );
 f.exec(env)
+  .then( env => console.log('done',env) )
   .then( env => console.log(f.names()) );
